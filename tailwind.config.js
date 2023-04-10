4586422f2be69abb51ea3ad349cda9c833b3d9e7
ibnumahjub <ibnumahjub23@gmail.com>
2023-04-10 14:10:39 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#FF1F57',
+        secondary: '#64748b',
+        dark: '#1A1A1A',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
